b411246aa866cc406d53ba2392f3223928e608e4
tobias.wittemann <tobias.wittemann@gmx.net>
2025-12-29 17:35:27 +0100

    Implementation of reading coords from input, calculation of distance matrix and calculation of acceleration

---
+++ project3/MD.f90
@@ -0,0 +1,75 @@
+program MD
+implicit none
+integer :: Natoms, i, j
+double precision :: dx2, dy2, dz2, m, dist, epsilon, sigma
+double precision, allocatable :: coord(:,:), mass(:)
+double precision, allocatable :: distance(:,:), acceleration(:,:)
+
+! Open and read input file
+open(1, file='inp.txt', status="old", action='read')
+read(1,*) Natoms    ! read number of atoms
+read(1,*)           ! read comment line
+write(*,*) "Number of atoms", Natoms
+allocate(coord(Natoms, NAtoms), mass(NAtoms))
+do i=1,Natoms
+  read(1,*) coord(i,1), coord(i,2), coord(i,3), mass(i)    ! read coordinates and mass line by line
+end do
+close(1)
+
+allocate(distance(NAtoms,NAtoms))
+
+! Compute distances
+call compute_distances(NAtoms, coord, distance)
+do i=1,NAtoms
+  write(*,*) distance(i,:)
+end do
+
+! Compute acceleration
+epsilon = 0.997 ! kJ/mol
+sigma = 3.405   ! Angstrom
+allocate(acceleration(NAtoms,NAtoms))
+call compute_acc(Natoms, epsilon, sigma, coord, mass, distance, acceleration)
+write(*,*) acceleration
+end program MD
+
+
+subroutine compute_distances(NAtoms, coord, distance)
+integer,intent(in) :: NAtoms
+integer :: i,j
+double precision :: dx2, dy2, dz2, m, dist
+double precision, intent(in) :: coord(NAtoms,NAtoms)
+double precision, intent(out) :: distance(NAtoms,NAtoms)
+do j=1,NAtoms
+  do i=1,j
+    write(*,*) 'i, j', i, j
+    dx2 = (coord(i,1)-coord(j,1))**2
+    dy2 = (coord(i,2)-coord(j,2))**2
+    dz2 = (coord(i,3)-coord(j,3))**2
+    dist = SQRT(dx2 + dy2 + dz2)
+    distance(i,j) = dist
+    distance(j,i) = dist
+  end do
+end do
+end subroutine compute_distances
+
+
+subroutine compute_acc(Natoms, epsilon, sigma, coord, mass, distance, acceleration)
+implicit none
+integer,intent(in) :: NAtoms
+integer :: i, j
+double precision, intent(in) :: coord(NAtoms,NAtoms), distance(NAtoms,NAtoms), mass(NAtoms), epsilon, sigma
+double precision, intent(out) :: acceleration(NAtoms,3)
+double precision, dimension(3) :: F, r, U_ij
+do i=1,NAtoms
+  write(*,*) 'i', i
+  F = 0
+  do j=1,NAtoms
+    if (i.ne.j) then
+      r = distance(i,j)
+      U_ij = 4*epsilon*((sigma/r)**12 - (sigma/r)**6)
+      F = F + U_ij*(coord(i,:)-coord(j,:))/r
+    end if
+  end do
+  acceleration(i,:) = -F/mass(i)
+end do
+end subroutine compute_acc

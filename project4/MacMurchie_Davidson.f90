4d1b01098dcae2187ba65ba58255551c0a89df1a
tobias.wittemann <tobias.wittemann@gmx.net>
2026-01-04 17:04:40 +0100

    Cleaned the code

--- project4/MacMurchie_Davidson.f90
+++ project4/MacMurchie_Davidson.f90
@@ -9,70 +9,21 @@ double precision,dimension(:,:), allocatable :: S
 double precision, parameter :: pi = acos(-1.0d0)
 
 ! Read basis set data from provided files
-Nbasis = 10
-maxcont = 3
-
+Nbasis = 10   ! Number of basis functions
+maxcont = 3   ! maximum degree of contraction (maxcont = 3 --> there are at most 3 primitive gaussians in one contracted basis function)
 allocate(basis_centers(Nbasis,3), primitive_exponents(Nbasis, maxcont), cartesian_exponents(Nbasis,maxcont), contraction_coeffs(Nbasis,maxcont))
+call read_array('basis_centers_sto3g.txt', Nbasis, 3, basis_centers)                        ! basis_centers contains the coordinates at which the basis functions are centered
+call read_array('primitive_exponents_sto3g.txt', Nbasis, maxcont, primitive_exponents)      ! primitive_exponents contains the exponents of the primitive basis functions
+call read_array('cartesian_exponents_sto3g.txt', Nbasis, 3, cartesian_exponents)            ! cartesian_exponents contains the exponents l,m,n (x^l*y^m*z^n*exp(-a*r^2)) indicating the angular momentum
+call read_array('contraction_coeffs_sto3g.txt', Nbasis, maxcont, contraction_coeffs)        ! contraction_coeffs contains the contraction coefficients for each basis function
 
-call read_array('basis_centers_sto3g.txt', Nbasis, 3, basis_centers)
-call read_array('primitive_exponents_sto3g.txt', Nbasis, maxcont, primitive_exponents)
-call read_array('cartesian_exponents_sto3g.txt', Nbasis, 3, cartesian_exponents)
-call read_array('contraction_coeffs_sto3g.txt', Nbasis, maxcont, contraction_coeffs)
-
-! Initialize overlap matrix
-allocate(S(NBasis,NBasis))
-S = 0.0d0
-
-! Iterate over contracted Gaussians
-do j=1,NBasis
-  rB = basis_centers(j,:)
-do i=1,j
-  rA = basis_centers(i,:)
-  rAB = rA - rB
-  S_ij = 0.0d0
-  ! Iterate over primitive Gaussians
-  do n=1,maxcont
-    dn = contraction_coeffs(j,n)
-    if (dn == 0.0d0) exit
-    b = primitive_exponents(j,n) 
-  do m=1,maxcont
-    dm = contraction_coeffs(i,m)
-    if (dm == 0.0d0) exit
-    a = primitive_exponents(i,m)
-    p = a + b
-    mu = a*b/p
-    rP = (a*rA + b*rB)/p
-    rPA = rP-rA
-    rPB = rP-rB
-
-  S_prim = dm*dn
-  ! Iterate over x,y,z
-  do d=1,3
-    i_max = cartesian_exponents(i,d)
-    j_max = cartesian_exponents(j,d)
-    t_max = i_max + j_max
-    xPA = rPA(d)
-    xPB = rPB(d)
-    xAB = rAB(d)
-    allocate(E_tij(0:t_max, 0:i_max+1, 0:j_max+1))
-    call calc_E_tij(i_max, j_max, p, mu, xAB, xPA, xPB, E_tij)     
-    Nm = 1/SQRT(double_factorial(REAL(2*i_max-1,kind=8))/((4*a)**i_max)*SQRT(pi/(2*a)))
-    Nn = 1/SQRT(double_factorial(REAL(2*j_max-1,kind=8))/((4*b)**j_max)*SQRT(pi/(2*b)))
-    S_prim = S_prim*Nm*Nn*E_tij(0,i_max,j_max)*SQRT(pi/p)
-    deallocate(E_tij)
-  end do
-  S_ij = S_ij + S_prim
-  end do
-  end do
-  S(i,j) = S_ij
-  S(j,i) = S_ij
-end do
-end do
-
+! Calculate overlap matrix
+allocate(S(Nbasis,Nbasis))
+call calc_S(Nbasis, maxcont, basis_centers, primitive_exponents, cartesian_exponents, contraction_coeffs,S)
+write(*,*) "Second test of overlap matrix"
 call write_array(S,Nbasis,Nbasis)
 
 end program MacMurchieDavidson
-
 
 subroutine write_array(arr, m, n)
 integer, intent(in) :: m, n
@@ -97,7 +48,7 @@ do i=1,m
 end do
 end subroutine read_array
 
-
+! This subroutine calculates the MacMurchie-Davidson coefficients E_ti. The coefficients are calculated up to i = i_max and j = j_max
 subroutine calc_E_tij(i_max, j_max, p, mu, xAB, xPA, xPB, E_tij)
 implicit none
 integer, intent(in) :: i_max, j_max
@@ -123,7 +74,7 @@ do i=0,i_max
 end do
 end subroutine calc_E_tij
 
-
+! This function calculates the double factorial of a given double-precision real
 function double_factorial(n_in) result(res_out)
 implicit none
 double precision, intent(in) :: n_in
@@ -152,5 +103,73 @@ else if ((n<0).and.(mod(n,2)==-1)) then
    res_out = 1/real(res, kind=8)
 end if
 end function double_factorial
+
+subroutine calc_S(Nbasis, maxcont, basis_centers, primitive_exponents, cartesian_exponents, contraction_coeffs,S)
+implicit none
+integer, intent(in) :: Nbasis, maxcont
+double precision, dimension(Nbasis,maxcont), intent(in) :: primitive_exponents, contraction_coeffs
+double precision, dimension(Nbasis,3), intent(in) :: basis_centers, cartesian_exponents
+double precision, dimension(Nbasis,Nbasis), intent(out) :: S
+integer :: i, j, t, n, m, d, i_max, j_max, t_max
+double precision, dimension(3) :: rA, rB, rAB, rPA, rPB, rP
+double precision :: S_ij, dn, dm, a, b, p, mu, Nm, Nn, xPA, xPB, xAB, S_prim, double_factorial
+double precision, dimension(:,:,:), allocatable :: E_tij
+double precision, parameter :: pi = acos(-1.0d0)
+
+! Initialize overlap matrix
+S = 0.0d0
+
+! Iterate over contracted Gaussians
+do j=1,NBasis
+  rB = basis_centers(j,:)
+do i=1,j
+  rA = basis_centers(i,:)
+  rAB = rA - rB
+  S_ij = 0.0d0
+  ! Iterate over primitive Gaussians
+  do n=1,maxcont
+    dn = contraction_coeffs(j,n)
+    if (dn == 0.0d0) exit         ! break from loop if contraction coefficient is zero (= all contributing primitives are through)
+    b = primitive_exponents(j,n)
+  do m=1,maxcont
+    dm = contraction_coeffs(i,m)
+    if (dm == 0.0d0) exit         ! break from loop if contraction coefficient is zero (= all contributing primitives are through)
+    a = primitive_exponents(i,m)
+    p = a + b
+    mu = a*b/p
+    rP = (a*rA + b*rB)/p
+    rPA = rP-rA
+    rPB = rP-rB
+
+  S_prim = dm*dn
+  ! Iterate over x,y,z
+  do d=1,3
+    i_max = cartesian_exponents(i,d)
+    j_max = cartesian_exponents(j,d)
+    t_max = i_max + j_max
+    xPA = rPA(d)
+    xPB = rPB(d)
+    xAB = rAB(d)
+    ! Expand Gaussian product in Hermite Gaussians using MacMurchie-Davidson Recurrence relations
+    allocate(E_tij(0:t_max, 0:i_max+1, 0:j_max+1))
+    call calc_E_tij(i_max, j_max, p, mu, xAB, xPA, xPB, E_tij)
+    ! Calculate normalization constant for primitive Gaussian for the given cartesian component
+    Nm = 1/SQRT(double_factorial(REAL(2*i_max-1,kind=8))/((4*a)**i_max)*SQRT(pi/(2*a)))
+    Nn = 1/SQRT(double_factorial(REAL(2*j_max-1,kind=8))/((4*b)**j_max)*SQRT(pi/(2*b)))
+    ! Multiply current value of primitive overlap with contributing factor form the given cartesian component
+    S_prim = S_prim*Nm*Nn*E_tij(0,i_max,j_max)*SQRT(pi/p)
+    deallocate(E_tij)
+  end do
+  ! Add overlap between primitives m,n to total overlap between basis functions i,j
+  S_ij = S_ij + S_prim
+  end do
+  end do
+  ! Fill in overlap matrix using symmetry
+  S(i,j) = S_ij
+  S(j,i) = S_ij
+end do
+end do
+end subroutine calc_S
+
 
 
